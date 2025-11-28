1cb0ed93d9af352ce5c6c2e4a717daafbd70589a
Andy de Oliveira <elderromero565@gmail.com>
2025-11-27 21:27:21 -0300

    edssdd

--- src/components/perfil/perfil.jsx
+++ src/components/perfil/perfil.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-import { API_URL } from "../../config";
+import { API_URL } from "../../../config";
 import "./perfil.css";
 
 import PainelComercial from "./components/painelcomercial";

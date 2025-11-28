adbd15b0147bbdb1aaee6f4d5d6dda680c002496
Andy de Oliveira <elderromero565@gmail.com>
2025-11-27 21:09:24 -0300

    eddd

--- src/config.jsx
+++ src/config.jsx
@@ -1,1 +1,7 @@
-export const API_URL = "http://localhost:8000";
+const isLocalhost =
+    window.location.hostname === "localhost" ||
+    window.location.hostname === "127.0.0.1";
+
+export const API_URL = isLocalhost
+    ? "http://localhost:8000"
+    : "https://alexsia-utilidades-backend.onrender.com";
